5536c3702a68cf1e253f06d15fbfe067aa9ade9f
popcorn6996 <ediyiechris@gmail.com>
2024-03-04 11:36:31 +0000

    Heroku Host

---
+++ speedapi.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/test-internet-speed', methods=['GET'])
+def test_internet_speed():
+    import speedtest
+    
+    st = speedtest.Speedtest()
+    
+    print('Testing Internet Speed')
+    
+    download_speed = st.download() / 10 **6
+    upload_speed = st.upload() / 10 **6
+    ping_speed = st.results.ping
+    
+    result = {
+        'download_speed': f'{download_speed:.2f} Mbps',
+        'upload_speed': f'{upload_speed:.2f} Mbps',
+        'ping_speed': f'{ping_speed:.2f} ms'
+    }
+    
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
